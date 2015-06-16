2e65ac3621dfed77bf022b8e311fb907424f4cd8
vaogun <vaogun@gmail.com>
2015-06-15 20:49:55 -0500

    Add "creating explicit intents at runtime" on page 379

--- app/src/main/java/com/vaojr/android/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/com/vaojr/android/nerdlauncher/NerdLauncherFragment.java
@@ -1,6 +1,7 @@
 package com.vaojr.android.nerdlauncher;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.Collections;
@@ -56,5 +58,18 @@ public class NerdLauncherFragment extends ListFragment {
         };
 
         setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        ResolveInfo resolveInfo = (ResolveInfo)l.getAdapter().getItem(position);
+        ActivityInfo activityInfo = resolveInfo.activityInfo;
+
+        if (activityInfo == null) return;
+
+        Intent i = new Intent(Intent.ACTION_MAIN);
+        i.setClassName(activityInfo.applicationInfo.packageName, activityInfo.name);
+
+        startActivity(i);
     }
 }
